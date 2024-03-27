4560fc3c05ecc65a6b4cc18173082552cc95187b
alumno <alumno@Instituto.local>
2024-03-27 08:24:34 -0300

    realice las primeras 2 act

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+Escribe un programa que compare dos números y muestre un mensaje indicando cuál es el mayor.
+*/
+let NumeroUno = prompt("Ingrese su primer número")
+let NumeroDos = prompt("Ingrese su segundo número")
+
+if (NumeroUno > NumeroDos) {
+    alert(NumeroUno + " es mayor")
+} else if (NumeroUno < NumeroDos) {
+    alert(NumeroDos + " es mayor")
+} else{
+    alert("Son iguales")
+}
+
+/*
+Crea un programa que determine si un número es positivo, negativo o cero.
+*/
+
+let numero = prompt("Ingrese un número")
+
+if (numero > 0) {
+    alert("Este número es positivo")
+} else if (numero < 0) {
+    alert("Este número es negativo")
+} else {
+    alert("Es cero")
+}
+
+/*
+Escribe un programa que verifique si un número es par o impar.
+Crea un programa que solicite la edad del usuario y muestre un mensaje indicando si es mayor de edad o no.
+/
+*/
+/*
+Escribe un programa que pida al usuario dos números y determine si son iguales o diferentes.
+*/
+/*
+Crea un programa que solicite un número del 1 al 7 y muestre el día de la semana correspondiente.
+*/
+/*
+Escribe un programa que determine si un año es bisiesto o no.
+ */
